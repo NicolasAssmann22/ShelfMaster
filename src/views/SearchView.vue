f31e7046ec9fa52f8933d483f49131f9ca6fb220
simonfliegel <sfliegel.dev@gmail.com>
2024-12-03 15:44:08 +0100

    initial search view commit

---
+++ src/views/SearchView.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import SearchBar from '../components/search/SearchBar.vue';
+</script>
+
+<template>
+  <div class="search-view">
+    <SearchBar />
+  </div>
+</template>
+
+<style scoped></style>
