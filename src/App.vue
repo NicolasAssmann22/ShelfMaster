6e8eea1c31e9fd2448dd2ddde9f8474f4042ddc5
simonfliegel <sfliegel.dev@gmail.com>
2024-12-05 09:27:42 +0100

    added back home link

--- src/App.vue
+++ src/App.vue
@@ -10,6 +10,7 @@ import HelloWorld from './components/HelloWorld.vue'
       <HelloWorld msg="You did it!" />
 
       <nav>
+        <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
         <RouterLink to="/search">Search</RouterLink>
       </nav>
