5c53530d2918d5b25395e43fa8fd1c5a343b5901
simonfliegel <sfliegel.dev@gmail.com>
2024-12-16 11:27:11 +0100

    trigger search when enter is pressed

--- src/components/search/SearchBar.vue
+++ src/components/search/SearchBar.vue
@@ -14,7 +14,7 @@ const emitSearch = () => {
 
 <template>
   <header class="flex items-center p-4 bg-purple-100 rounded-lg">
-    <input v-model="inputRef" type="text" placeholder="Gegenstand suchen..." class="flex-1 px-4 py-2 text-sm border rounded-lg outline-none border-gray-300 focus:ring-2 focus:ring-purple-500" />
+    <input v-model="inputRef" @keyup.enter="emitSearch" type="text" placeholder="Gegenstand suchen..." class="flex-1 px-4 py-2 text-sm border rounded-lg outline-none border-gray-300 focus:ring-2 focus:ring-purple-500" />
     <button @click="emitSearch" class="ml-4 text-purple-500 hover:text-purple-700">
       <MagnifyingGlassIcon class="h-6 w-6 text-blue-500" />
     </button>
