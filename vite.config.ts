85f27bf9c5da1c0bf72ff156b02db3765c5fa6ab
simonfliegel <sfliegel.dev@gmail.com>
2025-01-01 14:37:47 +0100

    change base path for gh-pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: './',
+  base: process.env.VITE_BASE_URL || './',
   plugins: [
     vue(),
     vueDevTools(),
