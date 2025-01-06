bbb73c21fa8ed638e71bad40fafae66cd4d0617e
Riesenoger <97044956+Riesenoger@users.noreply.github.com>
2025-01-06 11:22:29 +0100

    fieldlabel added props

--- src/components/fields/FieldLabel.vue
+++ src/components/fields/FieldLabel.vue
@@ -7,6 +7,13 @@
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const props = defineProps({
+  id: {
+    type: String,
+    required: true,
+  },
+})
+</script>
 
 <style scoped></style>
